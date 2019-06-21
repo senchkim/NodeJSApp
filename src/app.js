c74ea61ab9cb25083ad4016d9f5adb180d2b9b09
Dmitriy <Malnev.Dmitriy@wildberries.ru>
2019-06-21 16:34:04 +0300

    Add template in App

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
 const path = require(`path`);
 const express = require(`express`);
+const hbs = require(`hbs`);
 const PORT_APP = 3001;
 
 const app = express();
 
+// Define path for Express Config
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static server
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Harry Potter'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About page!',
+    name: 'Chris'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help page!',
+    name: 'Hello Help)'
+  });
+});
 
 app.get('', (req, res) => {
   res.send('');
@@ -13,6 +45,13 @@ app.get('', (req, res) => {
 
 app.get('/weather', (req, res) => {
   res.send('Weather page!');
+});
+
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    errorMessage: 'Oops, Sorry!'
+  })
 });
 
 app.listen(PORT_APP, () => {
